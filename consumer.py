0c417ac33abf40ef965b3516190a12699b8bffa7
mfbenko <mfbenko@gmail.com>
2025-01-09 17:18:43 -0500

    Functioning producer and consumer

--- consumer.py
+++ consumer.py
@@ -1,3 +1,4 @@
+#edited by Alec Ippolito and Max Benko
 #neccesary headers for functions
 import json
 
@@ -11,26 +12,29 @@ from pymongo import MongoClient
 MONGO_URI = 'mongodb://localhost:27017/'
 #Name of the database we are using
 DATABASE_NAME = 'kafka_web_attack_data'
+#Name of the collection that messages will be stored into within the database
 COLLECTION_NAME = 'consumer_records'
 
 #Set up MongoDB Client
 mongo_client = MongoClient(MONGO_URI)
 #set up thje mongo database
 db = mongo_client[DATABASE_NAME]
+#set up thje mongo collection
 collection = db[COLLECTION_NAME] 
 
 #Create a Kafka consumer instance with configuration
 consumer = KafkaConsumer(
 	#this is the kafaka topic to pull from
     'my_topic',
+	#must specify the api version used
 	api_version=(0,11,5),
 	#Address of the Kafka broker
     bootstrap_servers=['localhost:9092'],
 	#first message in will be first message read
     auto_offset_reset='earliest', 
-	#Automatically commit offsets after consuming messages
+	#automatically commit offsets after consuming messages
     enable_auto_commit=True,
-	#Consumer group ID to allow multiple consumers to balance the load
+	#consumer group ID to allow multiple consumers to balance the load
     group_id='my-group',
 	#switch from JSON objects to python objecets
     value_deserializer=lambda x: json.loads(x.decode('utf-8')) 
@@ -42,5 +46,4 @@ for WebAttack in consumer:
     #begin pymongo insertion into MongoDB
 	data = WebAttack.value
 	#Insert data into MongoDB
-	print(data)
 	collection.insert_one(data)
--- producer.py
+++ producer.py
@@ -1,3 +1,4 @@
+#edited by Alec Ippolito and Max Benko
 #neccesary headers for functions
 import csv
 import json
@@ -7,21 +8,23 @@ import time
 #must install with pip install kafka-python-ng
 from kafka import KafkaProducer
 
-#open the database file with read only permision
+#open the csv database file with read only permision
 with open('csic_database.csv', 'r') as file:
 	#create reader object and begin reading one line at a time in loop and make each row a dictionary
 	csv_reader = csv.DictReader(file)
 	#create a list of dictionaries
 	rows = list(csv_reader)
 	
-#return the json information as byte encoded
+#function to convert Python dictionaries to JSON. Called implicitly via producer.send()
 def json_serializer(data):
+	#kafka only accepts byte encoded JSON
     return json.dumps(data).encode('utf-8')
 
 #Create a Kafka producer instance with configuration
 producer = KafkaProducer(
+	#speicify which API version we will be using
 	api_version=(0,11,5),
-	#Address of the Kafka broker
+	#Port of the Kafka broker
     bootstrap_servers=['localhost:9092'],
 	#set the serializer method to be the custom one we made
     value_serializer=json_serializer,
@@ -34,13 +37,10 @@ print("sending messages...")
 #begin the simulation with this loop
 for item in rows:
 	#generate a random number for simulation
-	print("1")
 	random_int = random.randint(1, 10)
 	#sleep inorder to simulate real world randomness
-	print("2")
 	time.sleep(random_int)	
-	#send the json item to the producer
-	print("3")
+	#send the item to the producer after serialization
 	producer.send(topic, value=item)
 #The messages have been sent...free up space
 producer.close()
