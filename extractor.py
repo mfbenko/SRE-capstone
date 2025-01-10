dc3a354449fa901c05907e91a0ebe85a3c279764
mfbenko <mfbenko@gmail.com>
2025-01-10 17:08:19 -0500

    basic summary record made

--- extractor.py
+++ extractor.py
@@ -1,5 +1,8 @@
-#necessary imports to get pull information out of the database
+#necessary import to get pull information out of the database
 from pymongo import MongoClient
+
+#pandas will be used so that we can make summary records
+import pandas
 
 #connecting mongodb to the correct port
 client = MongoClient('mongodb://localhost:27017/')
@@ -10,12 +13,23 @@ collection = db['consumer_records']
 
 #returns all messages in the database(may change later)
 results = collection.find()
+#take the results and put them into a pandas dataframe(table)
+pandasTable = pandas.DataFrame(results)
+	
+#prepare data for insertion into mySQL by creating summary records.
+#we will group data by normal/anomalous(represented by '') and http method
+summary_record = pandasTable.groupby(['','Method']).agg(
+	#how many attacks for each host
+	host_count=('host', 'count'),
+	#how many unique urls exist
+	unique_URLS=('URL','nunique'),
+	#is there an entry in the database where there is no Accept?
+	Accept_exists=('Accept','all')
+	#can add more here if we would like
+).reset_index()
+	
+#debug print
+print(summary_record)
 
-#loop throught the dictionary we just created and print to the screen
-for message in results:
-    print(message)
-	
-#prepare data for insertion into mySQL by creating summary records
-	
 #we did it... stop the connection
 client.close()
