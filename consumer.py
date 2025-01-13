30c5e2ed95742cd21699255827cbc4fb9762edc4
Alec Ippolito <ajippolito@live.com>
2025-01-13 14:02:49 -0500

    Cleaned up project before trying containerization

--- consumer.py
+++ consumer.py
@@ -1,49 +1,32 @@
 #edited by Alec Ippolito and Max Benko
-#neccesary headers for functions
 import json
-
-#must install with pip install kafka-python-ng
 from kafka import KafkaConsumer
-
-#must install with pip install pymongo
 from pymongo import MongoClient
 
-#connecting mongodb to the correct port
-MONGO_URI = 'mongodb://localhost:27017/'
-#Name of the database we are using
+# Define MongoDB Constants
+MONGO_URI = 'mongodb://localhost:27017/' # MongoDB Port
 DATABASE_NAME = 'kafka_web_attack_data'
-#Name of the collection that messages will be stored into within the database
 COLLECTION_NAME = 'consumer_records'
 
-#Set up MongoDB Client
+# MongoDB Client
 mongo_client = MongoClient(MONGO_URI)
-#set up thje mongo database
 db = mongo_client[DATABASE_NAME]
-#set up thje mongo collection
 collection = db[COLLECTION_NAME] 
 
-#Create a Kafka consumer instance with configuration
+# Kafka Topic and Consumer Object Creation
+topic = 'my_topic'
 consumer = KafkaConsumer(
-	#this is the kafaka topic to pull from
-    'my_topic',
-	#must specify the api version used
+    topic,
 	api_version=(0,11,5),
-	#Address of the Kafka broker
-    bootstrap_servers=['localhost:9092'],
-	#first message in will be first message read
+    bootstrap_servers=['localhost:9092'], # Kafka Broker
     auto_offset_reset='earliest', 
-	#automatically commit offsets after consuming messages
     enable_auto_commit=True,
-	#consumer group ID to allow multiple consumers to balance the load
     group_id='my-group',
-	#switch from JSON objects to python objecets
     value_deserializer=lambda x: json.loads(x.decode('utf-8')) 
 )
 
-#begin consuming
-print("Listening for messages...")
+# Extract data from broker and insert it into MongoDB
 for WebAttack in consumer:
-    #begin pymongo insertion into MongoDB
 	data = WebAttack.value
-	#Insert data into MongoDB
+	print(f"Recieving Data:\t\t{data}\n")
 	collection.insert_one(data)
--- producer.py
+++ producer.py
@@ -1,46 +1,34 @@
 #edited by Alec Ippolito and Max Benko
-#neccesary headers for functions
 import csv
 import json
 import random
 import time
-
-#must install with pip install kafka-python-ng
 from kafka import KafkaProducer
 
-#open the csv database file with read only permision
-with open('csic_database.csv', 'r') as file:
-	#create reader object and begin reading one line at a time in loop and make each row a dictionary
-	csv_reader = csv.DictReader(file)
-	#create a list of dictionaries
-	rows = list(csv_reader)
-	
-#function to convert Python dictionaries to JSON. Called implicitly via producer.send()
+# Serializes the input data to a JSON-formatted string and encodes it to UTF-8 bytes. Note: Called implicitly via producer.send()
 def json_serializer(data):
-	#kafka only accepts byte encoded JSON
     return json.dumps(data).encode('utf-8')
 
-#Create a Kafka producer instance with configuration
+# Open database and read content
+with open('csic_database.csv', 'r') as file:
+	csv_reader = csv.DictReader(file)
+	rows = list(csv_reader)
+	
+# Kafka Topic and Producer Object Creation
+topic = 'my_topic'
 producer = KafkaProducer(
-	#speicify which API version we will be using
 	api_version=(0,11,5),
-	#Port of the Kafka broker
     bootstrap_servers=['localhost:9092'],
-	#set the serializer method to be the custom one we made
     value_serializer=json_serializer,
 	)
 
-#This is the kafka topic where the messages will be sent
-topic = 'my_topic'
+# Send Messages
+for index, item in enumerate(rows):
+	time.sleep(random.randint(1, 10)) 
+	print(f"Sending Message {index}:\nProducer:\t{producer}\nTopic:\t\t{topic}\nContent:\t{item}\n")
+	producer.send(topic, value=item) 
+	producer.flush() # Ensure the message is sent immediatly
 
-print("sending messages...")
-#begin the simulation with this loop
-for item in rows:
-	#generate a random number for simulation
-	random_int = random.randint(1, 10)
-	#sleep inorder to simulate real world randomness
-	time.sleep(random_int)	
-	#send the item to the producer after serialization
-	producer.send(topic, value=item)
-#The messages have been sent...free up space
+# Cleanup
+print("Sending done...Cleanning up by closing producer!")
 producer.close()
