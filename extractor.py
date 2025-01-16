3e6f8e536cda4839ea3c5dbaee8793a541bedade
mfbenko <mfbenko@gmail.com>
2025-01-16 15:52:13 -0500

    MySQL insertion is functional

--- extractor.py
+++ extractor.py
@@ -1,35 +1,65 @@
 #necessary import to get pull information out of the database
 from pymongo import MongoClient
-
-#pandas will be used so that we can make summary records
 import pandas
+import mysql.connector
 
-#connecting mongodb to the correct port
+#connecting to the mongodb
 client = MongoClient('mongodb://localhost:27017/')
-#Name of the database we are using
 db = client['kafka_web_attack_data']
-#Name of the collection that messages are stored in
 collection = db['consumer_records']
 
-#returns all messages in the database(may change later)
+#returns all messages in the database and creates pandas data frame
 results = collection.find()
-#take the results and put them into a pandas dataframe(table)
 pandasTable = pandas.DataFrame(results)
 	
 #prepare data for insertion into mySQL by creating summary records.
-#we will group data by normal/anomalous(represented by '') and http method
 summary_record = pandasTable.groupby(['','Method']).agg(
-	#how many attacks for each host
 	host_count=('host', 'count'),
-	#how many unique urls exist
 	unique_URLS=('URL','nunique'),
-	#is there an entry in the database where there is no Accept?
 	Accept_exists=('Accept','all')
 	#can add more here if we would like
 ).reset_index()
 	
 #debug print
 print(summary_record)
+
+#create the sql connection
+MySQL = mysql.connector.connect(
+	host='localhost',
+	user='root',
+	password='root'
+)
+
+#create a cursor write the queries
+cursor = MySQL.cursor()
+
+#create a database and table if it does not exist
+cursor.execute("""CREATE DATABASE IF NOT EXISTS capstone_db""")
+cursor.execute("""USE capstone_db""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS summary(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	host_count INT,
+	unique_URLS INT,
+	Accept_exists TINYINT(1) NOT NULL DEFAULT 0
+)
+""")
+
+#insert the summary record into MySQL
+for _, row in summary_record.iterrows():
+    query = """
+    INSERT INTO summary (host_count, unique_URLS, Accept_exists)
+    VALUES (%s, %s, %s)
+    """
+    entry = (row['host_count'], row['unique_URLS'], int(row['Accept_exists']))
+    cursor.execute(query, entry)
+
+
+#Make the commit and exit MySQL
+MySQL.commit()
+cursor.close()
+MySQL.close()
 
 #we did it... stop the connection
 client.close()
