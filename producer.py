4ee176d3856576b0797309479cb760096606f4cb
Alec Ippolito <ajippolito@live.com>
2025-01-20 10:24:13 -0500

    Chagned up producer and added consumer - succesfully working

--- producer.py
+++ producer.py
@@ -5,36 +5,42 @@ import random
 import time
 from kafka import KafkaProducer
 
+# Serializes the input data to a JSON-formatted string and encodes it to UTF-8 bytes. Note: Called implicitly via producer.send()
+def json_serializer(data):
+    return json.dumps(data).encode('utf-8')
+
 class producer:
 	def __init__(self):
 		self.rows = ''
 		self.topic = 'my_topic'
+		self.producer = ''
 
-	# Serializes the input data to a JSON-formatted string and encodes it to UTF-8 bytes. Note: Called implicitly via producer.send()
-	def json_serializer(data):
-		return json.dumps(data).encode('utf-8')
-
-	def create_producer():
+	def create_producer(self):
 		# Open database and read content
 		with open('csic_database.csv', 'r') as file:
 			csv_reader = csv.DictReader(file)
 			self.rows = list(csv_reader)
 		
 		# Kafka Topic and Producer Object Creation
-		producer = KafkaProducer(
+		self.producer = KafkaProducer(
 			api_version=(0,11,5),
 			bootstrap_servers=['localhost:9092'],
 			value_serializer=json_serializer,
 			)
 
 	# Send Messages
-	def send_message():
+	def send_message(self):
 		for index, item in enumerate(self.rows):
 			time.sleep(random.randint(1, 10)) 
 			print(f"Sending Message {index}:\nProducer:\t{producer}\nTopic:\t\t{self.topic}\nContent:\t{item}\n")
-			producer.send(self.topic, value=item) 
-			producer.flush() # Ensure the message is sent immediatly
+			self.producer.send(self.topic, value=item) 
+			self.producer.flush() # Ensure the message is sent immediatly
 
 		# Cleanup
 		print("Sending done...Cleanning up by closing producer!")
 		producer.close()
+
+if __name__ == "__main__":
+    producer_obj = producer()
+    producer_obj.create_producer()
+    producer_obj.send_message() 
