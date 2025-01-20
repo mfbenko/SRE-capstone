fa14d6809fce9b88c00e174f9c81ce91990998ea
mfbenko <mfbenko@gmail.com>
2025-01-20 10:13:21 -0500

    made a class

--- producer.py
+++ producer.py
@@ -5,30 +5,36 @@ import random
 import time
 from kafka import KafkaProducer
 
-# Serializes the input data to a JSON-formatted string and encodes it to UTF-8 bytes. Note: Called implicitly via producer.send()
-def json_serializer(data):
-    return json.dumps(data).encode('utf-8')
+class producer:
+	def __init__(self):
+		self.rows = ''
+		self.topic = 'my_topic'
 
-# Open database and read content
-with open('csic_database.csv', 'r') as file:
-	csv_reader = csv.DictReader(file)
-	rows = list(csv_reader)
-	
-# Kafka Topic and Producer Object Creation
-topic = 'my_topic'
-producer = KafkaProducer(
-	api_version=(0,11,5),
-    bootstrap_servers=['localhost:9092'],
-    value_serializer=json_serializer,
-	)
+	# Serializes the input data to a JSON-formatted string and encodes it to UTF-8 bytes. Note: Called implicitly via producer.send()
+	def json_serializer(data):
+		return json.dumps(data).encode('utf-8')
 
-# Send Messages
-for index, item in enumerate(rows):
-	time.sleep(random.randint(1, 10)) 
-	print(f"Sending Message {index}:\nProducer:\t{producer}\nTopic:\t\t{topic}\nContent:\t{item}\n")
-	producer.send(topic, value=item) 
-	producer.flush() # Ensure the message is sent immediatly
+	def create_producer():
+		# Open database and read content
+		with open('csic_database.csv', 'r') as file:
+			csv_reader = csv.DictReader(file)
+			self.rows = list(csv_reader)
+		
+		# Kafka Topic and Producer Object Creation
+		producer = KafkaProducer(
+			api_version=(0,11,5),
+			bootstrap_servers=['localhost:9092'],
+			value_serializer=json_serializer,
+			)
 
-# Cleanup
-print("Sending done...Cleanning up by closing producer!")
-producer.close()
+	# Send Messages
+	def send_message():
+		for index, item in enumerate(self.rows):
+			time.sleep(random.randint(1, 10)) 
+			print(f"Sending Message {index}:\nProducer:\t{producer}\nTopic:\t\t{self.topic}\nContent:\t{item}\n")
+			producer.send(self.topic, value=item) 
+			producer.flush() # Ensure the message is sent immediatly
+
+		# Cleanup
+		print("Sending done...Cleanning up by closing producer!")
+		producer.close()
