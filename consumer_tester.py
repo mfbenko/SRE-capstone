cd421d0d598cda90ec691b4c59c6689d16912414
mfbenko <mfbenko@gmail.com>
2025-01-21 09:58:33 -0500

    started test bootstrap

--- consumer_tester.py
+++ consumer_tester.py
@@ -1,1 +1,16 @@
-#test the functionality of consumer.py
+#using unitest to test consumer.py functionality
+import unittest
+import csv
+import json
+from producer import KafkaProducerService
+from unittest.mock import patch, MagicMock
+
+class TestKafkaConsuner(unittest.TestCase):
+	def test_consume_messages(self):
+		#do something
+	def test_insert_into_mongodb(self):
+		#do something
+	def test_run(self):
+		#do something
+if __name__ == '__main__':
+	unittest.main()
